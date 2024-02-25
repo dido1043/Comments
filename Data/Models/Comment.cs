eed1d0ceca81f324b7c25988a55c42f4774441cf
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-02-25 15:39:31 +0200

    up

---
+++ Data/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace Comments.Data.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string CommentText { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+
+    }
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Comments.Data.Models
+{
+    public class User : IdentityUser
+    {
+        IList<UserLoginInfo> Logins { get; set; }
+    }
+}
